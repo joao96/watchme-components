837db4fbc0982d0e492c4e88a2206eaeb288c887
joao96 <jvpoletti@gmail.com>
2021-10-01 11:56:22 -0300

    🎨 Content and Sidebar as components

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from "../@types/GenreResponseProps";
+import { Button } from "./Button";
+
+type SideBarProps = {
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+  genres: GenreResponseProps[];
+};
+
+export function SideBar({
+  genres,
+  handleClickButton,
+  selectedGenreId,
+}: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
